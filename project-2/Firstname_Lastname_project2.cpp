a65faf8a0648862229c35579607dca36aa4756fb
Alexander R. Block <arblock@uic.edu>
2026-09-22 09:32:56 -0500

    Fixed Merkle Tree description.

--- project-2/Firstname_Lastname_project2.cpp
+++ project-2/Firstname_Lastname_project2.cpp
@@ -225,7 +225,7 @@ string merkle_commit(const vector<string>& list, function<string(string)> hash_f
  *      - Intuitively, you can construct the proof as follows
  *          - In the complete binary tree representing the merkle root computation, 
  *              draw a leaf-to-root path the leaf list[i] to the root.
- *          - Append [list[i], "i"] to the proof.
+ *          - Append list[i] to the proof.
  *          - For every node on the root-to-leaf path that is not a leaf node:
  *              - add the label (i.e., hash) of its child that is NOT on the 
  *                  root to leaf path to the proof.
@@ -314,8 +314,8 @@ vector<string> merkle_open_position(
  *  Algorithm:
  *      - Given the string proof, you must now verify that it is consistent with the root.
  *      - Assuming the proof is in the correct order, verification proceeds as follows:
- *          - compute h = hash_function(proof[0]+proof[1])
- *          - for p in proof[2:] (i.e., to the end of the proof)
+ *          - compute h = hash_function(proof[0]+"i")
+ *          - for p in proof[1:] (i.e., to the end of the proof)
  *              - determine whether p is the left or right input to the hash function
  *              - compute h = hash_function(p || h) or hash_function(h || p) based on
  *                  the above decision
